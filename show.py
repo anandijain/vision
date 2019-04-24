cfdffa6104f0e0c93aed6c343be2a89e8277cd3e
anandijain <anandj@uchicago.edu>
2019-04-24 13:52:50 -0500

    cleaned repo for readability

--- show.py
+++ show.py
@@ -1,28 +1,15 @@
 import cv2
 import numpy as np
 import time
-# import pygame
 
-# W = 1920 // 2
-# H = 1080 // 2
-
-# screen = pygame.display.set_mode((W, H))
-
-
-# cv2.namedWindow('image', cv2.WINDOW_NORMAL)
+PATH = 'images/cvtest.jpg'
 
 
 if __name__ == "__main__":
-    img = cv2.imread('cvtest.jpg')
-    # img_cp = np.copy(cv2.resize(img, (W, H)))
-    # gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-    # blur = cv2.GaussianBlur(gray, (11, 11), 0)
-    # canny = cv2.Canny(gray, 150, 300)
-    res = cv2.resize(img, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_CUBIC)
-    # cv2.imwrite('output_contour.png',  canny)
-    # cv2.imwrite('output_contour_resized.png',  res)
-    cv2.imshow('canny', res)
+    img = cv2.imread('images/cvtest.jpg')
+    res = cv2.resize(canny, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_CUBIC)
+
+    cv2.imshow('resized', res)
     cv2.waitKey(0)
 
-    # pygame.display.flip()
     print(img.shape)  
