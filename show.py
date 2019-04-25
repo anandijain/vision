0af7be89f925f7138b06e00b614d42b1a2c9ff47
anandijain <anandj@uchicago.edu>
2019-04-25 18:14:56 -0500

    working on videos

--- show.py
+++ show.py
@@ -7,7 +7,7 @@ PATH = 'images/cvtest.jpg'
 
 if __name__ == "__main__":
     img = cv2.imread('images/cvtest.jpg')
-    res = cv2.resize(canny, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_CUBIC)
+    res = cv2.resize(img, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_CUBIC)
 
     cv2.imshow('resized', res)
     cv2.waitKey(0)
